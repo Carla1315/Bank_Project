8723ffcf3e32e242df09c08f78362d79514cdfa6
prueba <prueba@gmail1.com>
2022-02-02 20:25:53 -0400

    Unit Test

--- src/Account.spec.ts
+++ src/Account.spec.ts
@@ -1,9 +1,17 @@
 import { Current_Count } from "./Current_Count";
+import { Customer } from "./Customer";
 import * as clientes from './json_examples/Customer.json';
 
 test('Enter Money', () => {
-    /*const moneyInAccount = Math.random();
+    const moneyInAccount = Math.random();
     const moneytoEnter = Math.random();
-    const account = new Current_Count(Math.random().toString(),moneyInAccount,null);
-    expect(account.enterMoney(moneytoEnter)).toBe(moneyInAccount + moneytoEnter)*/
+    const clienteGeneral = new Customer (
+        clientes[0].id.toString(), 
+        clientes[0].name, 
+        clientes[0].last_name, 
+        clientes[0].address, 
+        clientes[0].phone.toString(), 
+        clientes[0].cellPhone.toString())
+    const account = new Current_Count(Math.random().toString(),moneyInAccount,clienteGeneral);
+    expect(account.enterMoney(moneytoEnter)).toBe(moneyInAccount + moneytoEnter)
 })
